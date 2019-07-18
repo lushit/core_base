a2fa521f476ee0f4a532f0bc85f907619637bf44
with <with@lush.co.kr>
2019-07-18 10:56:53 +0900

    default form

--- src/main/java/lush/config/DataBaseConfig.java
+++ src/main/java/lush/config/DataBaseConfig.java
@@ -15,6 +15,12 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 @Configuration
 public class DataBaseConfig {
 	
+	/**
+	 * sqlSessionFactory 을 정의하여 Bean에 등록한다.
+	 * @param dataSource
+	 * @return sessionFactory.getObject()
+	 * @throws Exception
+	 */
 	@Bean
 	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
 		final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
@@ -24,11 +30,21 @@ public class DataBaseConfig {
 		return sessionFactory.getObject();
 	}
 	
+	/**
+	 * transactionManager 이름의 DataSourceTransactionManager를 Bean에 등록한다.
+	 * @param dataSource
+	 * @return DataSourceTransactionManager
+	 */
 	@Bean(name="transactionManager")
 	public DataSourceTransactionManager mssqlTransactionManager(DataSource dataSource) {
 		return new DataSourceTransactionManager(dataSource);
 	}
 	
+	/**
+	 * sqlSession 이름의 SqlSessionTemplate을 Bean에 등록한다.
+	 * @param sqlSessionFactory
+	 * @return SqlSessionTemplate
+	 */
 	@Bean(name="sqlSession")
 	public SqlSessionTemplate sqlSession(SqlSessionFactory sqlSessionFactory){
 		return new SqlSessionTemplate(sqlSessionFactory);
